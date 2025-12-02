758a6ce5ba77e71c270cf7f9b8199d6ce694c326
octaviorivas240 <21150373@aguascalientes.tecnm.mx>
2025-12-02 08:39:26 -0600

    c

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Unit;
 
-use App\Http\Controllers\OperationsController;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /**
+     * A basic test example.
+     */
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
@@ -16,9 +18,10 @@ class ExampleTest extends TestCase
     {
         $controller = new OperationsController;
 
-        $this->assertIsInt($controller->addition(5, 6));
-        $this->assertNotNull($controller->addition(5, 6));
+        $this->assertIsInt($controller->addition(a: 5, b: 6));
 
-        $this->assertGreaterThan(5, $controller->addition(5, 6));
+        $this->assertNotNull($controller->addition(a: 5, b: 6));
+
+        $this->assertGreaterThan(minimum: 5, $controller->addition(a: 5, b: 6));
     }
 }
