c605ba6fb3720b37944f1211f2027e6bf6d397e6
octaviorivas240 <21150373@aguascalientes.tecnm.mx>
2025-12-02 09:22:22 -0600

    cambios

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic test example 1.
      */
     public function test_that_true_is_true(): void
     {
