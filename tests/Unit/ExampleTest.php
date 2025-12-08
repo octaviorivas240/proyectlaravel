1e98f80fffb2e7c8da8147d1cb04483cc0b831cb
Monica Vera <MonicaVera2002@github.com>
2025-12-07 21:21:07 -0600

    Función de Mónica: Calcular el IMC (Índice de Masa Corporal)

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -25,4 +25,17 @@ class ExampleTest extends TestCase
 
         $this->assertGreaterThan(5, $controller->addition(5, 6));
     }
+
+    /**
+     * Función de Mónica, calcular el IMC.
+     */
+    public function test_calcularIMC(): void
+    {
+        $controller = new OperationsController();
+
+        $resultado = $controller->calcularIMC(62, 1.65);
+
+        $this->assertEquals(22.86, $resultado['El IMC es: ']);
+        $this->assertEquals('Normal', $resultado['Corresponde a la categoria:']);
+    }
 }
